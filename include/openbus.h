ebc150d95bf6790290cb115a4cbef7915f32ea68
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2012-06-19 11:31:06 +0000

    SDK C++
    + Documentação do módulo openbus.h.
     - Reuso dos textos contidos em openbus.idl.
     - Comentários específicos da linguagem C++.
     
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@130100 ae0415b3-e90b-0410-900d-d0be9363c56b

--- include/openbus.h
+++ include/openbus.h
@@ -1,3 +1,8 @@
+/**
+* \mainpage API - SDK Openbus C++
+* \file openbus.h
+*/
+
 #ifndef TECGRAF_OPENBUS_H_
 #define TECGRAF_OPENBUS_H_
 
@@ -5,7 +10,28 @@
 #include <manager.h>
 #include <log.h>
 
+/**
+* \brief openbus
+*/
 namespace openbus {
+  /**
+	* Inicializa um ORB utilizado exclusivamente para uso dentro de barramentos OpenBus. É possível 
+	* obter um ConnectionManager através do resolve_initial_reference("OpenBusConnectionManager").
+	*
+  * Uma instância de ORBInitializer é criada e um par de interceptadores cliente/servidor são 
+  * registrados. O RootPOA é ativado.
+  *
+  * Atenção: O Mico 2.3.13 não permite a inicialização de mais de um ORB. Após uma primeira chamada 
+  * bem sucedida de initORB(), todas as chamadas consecutivas retornarão o mesmo ORB que foi criado
+  * na primeira chamada.
+  * 
+  * @param[in] argc Número de parâmetros usados na inicialização do ORB.
+  * @param[in] argv Parâmetros usados na inicialização do ORB.
+  * 
+  * @throw CORBA::Exception
+  *
+  * @return O ORB inicializado.
+	*/
   CORBA::ORB* initORB(int argc, char** argv) throw(CORBA::Exception);
 }
 
