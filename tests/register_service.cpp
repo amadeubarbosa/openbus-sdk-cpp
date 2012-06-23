0e9778c8a6d92cc60d2893a658d4054b4c34e372
Felipe Magno Almeida <felipema@tecgraf.puc-rio.br>
2012-06-23 21:24:42 +0000

    [OPENBUS-1707] [CPP] Implementar testes para o SDK 2.0  - Conserto de teste em multithread
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@130277 ae0415b3-e90b-0410-900d-d0be9363c56b

--- tests/register_service.cpp
+++ tests/register_service.cpp
@@ -3,6 +3,10 @@
 #include "stubs/hello.h"
 #include <scs/ComponentContext.h>
 #include <configuration.h>
+#ifdef OPENBUS_SDK_MULTITHREAD
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#endif
 
 struct hello_impl : public POA_Hello
 {
@@ -23,15 +27,27 @@ struct hello_impl : public POA_Hello
   bool servant_called;
 };
 
+#ifdef OPENBUS_SDK_MULTITHREAD
+void call_orb(CORBA::ORB_var orb)
+{
+  orb->run();
+}
+#endif
+
 int main(int argc, char** argv)
 {
   openbus::configuration cfg(argc, argv);
   CORBA::ORB_var orb = openbus::initORB(argc, argv);
 
+#ifdef OPENBUS_SDK_MULTITHREAD
+  boost::thread orb_thread(boost::bind(&call_orb, orb));
+#endif
+
   CORBA::Object_ptr obj_connection_manager = orb->resolve_initial_references("OpenbusConnectionManager");
   openbus::ConnectionManager* manager = dynamic_cast<openbus::ConnectionManager*>(obj_connection_manager);
   std::auto_ptr <openbus::Connection> conn (manager->createConnection(cfg.host().c_str(), cfg.port()));
   conn->loginByPassword(cfg.user().c_str(), cfg.password().c_str());
+  manager->setDefaultConnection(conn.get());
   
   scs::core::ComponentId componentId;
   componentId.name = "Hello";
@@ -39,18 +55,30 @@ int main(int argc, char** argv)
   componentId.minor_version = '0';
   componentId.patch_version = '0';
   componentId.platform_spec = "";    
-  scs::core::ComponentContext ctx(manager->orb(), componentId);
+  {
+    CORBA::Object_var poa_obj = orb->resolve_initial_references("RootPOA");
+    PortableServer::POA_var poa = PortableServer::POA::_narrow(poa_obj);
+    PortableServer::POAManager_var poa_manager = poa->the_POAManager();
+    poa_manager->activate();
 
-  bool servant_called = false;
-  hello_impl hello_servant (*conn, servant_called);
+    scs::core::ComponentContext ctx(manager->orb(), componentId);
 
-  ctx.addFacet("hello", "IDL:Hello:1.0", &hello_servant);
+    bool servant_called = false;
+    hello_impl hello_servant (*conn, servant_called);
 
-  openbus::idl_or::ServicePropertySeq props;
-  props.length(1);
-  openbus::idl_or::ServiceProperty property;
-  property.name = "offer.domain";
-  property.value = "OpenBus Demos";
-  props[0] = property;
-  conn->offers()->registerService(ctx.getIComponent(), props);
+    ctx.addFacet("hello", "IDL:Hello:1.0", &hello_servant);
+
+    openbus::idl_or::ServicePropertySeq props;
+    props.length(1);
+    openbus::idl_or::ServiceProperty property;
+    property.name = "offer.domain";
+    property.value = "OpenBus Demos";
+    props[0] = property;
+    conn->offers()->registerService(ctx.getIComponent(), props);
+  }
+
+#ifdef OPENBUS_SDK_MULTITHREAD
+  orb->shutdown(true);
+  orb_thread.join();
+#endif
 }
