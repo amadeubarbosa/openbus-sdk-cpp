3a5b103b0eba4d0c7a1defe6970a98ee1d2aeda9
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2012-08-21 13:50:59 +0000

    OPENBUS-1973 Permitir uso da estrutura AutoLock em singlethread e criar um wrapper Mutex.
    + Adição do header Mutex_impl.h.
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@132460 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ include/openbus/util/Mutex_impl.h
@@ -0,0 +1,30 @@
+#ifndef TECGRAF_MUTEX_H_
+#define TECGRAF_MUTEX_H_
+
+#include <CORBA.h>
+
+namespace openbus {
+class Mutex {
+public:
+  void lock() {
+    #ifdef OPENBUS_SDK_MULTITHREAD
+    _mutex.lock();
+    #endif
+  }
+
+  void unlock() {
+    #ifdef OPENBUS_SDK_MULTITHREAD
+    _mutex.unlock();
+    #endif
+  }
+
+  #ifdef OPENBUS_SDK_MULTITHREAD
+  MICOMT::Mutex * mutex() { return &_mutex; }
+  #endif
+private:
+  #ifdef OPENBUS_SDK_MULTITHREAD
+  MICOMT::Mutex _mutex;
+  #endif
+};
+}
+#endif
