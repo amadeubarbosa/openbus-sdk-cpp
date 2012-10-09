5bfbbc826402acc02bce12a207b5eb60f3fabdeb
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2012-10-09 13:54:07 +0000

    OPENBUS-1845 [C++] Thread de renovacao nao renova o login apos uma nova conexao efetuada por LeaseExpiredCallback.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/OB_SDK_CPP_v1_05_04_2011_07_29@134276 ae0415b3-e90b-0410-900d-d0be9363c56b

--- demo/hello/mico/server.cpp
+++ demo/hello/mico/server.cpp
@@ -31,13 +31,72 @@ struct HelloImpl : virtual public POA_demoidl::hello::IHello {
   };
 };
 
+void loginAndRegister() {
+  std::cout << "Conectando no barramento..." << std::endl;
+
+/* Conexao com o barramento atraves de certificado. */
+  try {
+    registryService = bus->connect(entityName, privateKeyFilename, ACSCertificateFilename);
+  } catch (const CORBA::SystemException &) {
+    std::cout << "** Nao foi possivel se conectar ao barramento. **" << std::endl
+              << "* Falha na comunicacao. *" << std::endl;
+    exit(1);
+  } catch (const openbus::LOGIN_FAILURE &) {
+    std::cout << "** Nao foi possivel se conectar ao barramento. **" << std::endl
+              << "* Par usuario/senha invalido. *" << std::endl;
+    exit(1);
+  } catch (const openbus::SECURITY_EXCEPTION &e) {
+    std::cout << e.what() << std::endl;
+    exit(1);
+  }
+
+  std::cout << "Conexao com o barramento estabelecida com sucesso!" << std::endl;
+
+/* Definicao do componente. */
+  scs::core::ComponentId componentId;
+  componentId.name = "HelloComponent";
+  componentId.major_version = '1';
+  componentId.minor_version = '0';
+  componentId.patch_version = '0';
+  componentId.platform_spec = "nenhuma";
+
+  ::componentContext = new scs::core::ComponentContext(bus->getORB(), componentId);
+
+  std::auto_ptr<PortableServer::ServantBase> facet(new HelloImpl);
+  ::componentContext->addFacet(facetName, "IDL:demoidl/hello/IHello:1.0", facet);
+  assert(!facet.get());
+
+  openbus::util::PropertyListHelper propertyListHelper;
+
+/* Criacao de uma *oferta de servico*. */
+  tecgraf::openbus::core::v1_05::registry_service::ServiceOffer serviceOffer;
+  serviceOffer.properties = propertyListHelper.getPropertyList();
+  serviceOffer.member = componentContext->getIComponent();
+
+  std::cout << "Registrando servico IHello no barramento..." << std::endl;
+
+/* Registro do servico no barramento. */
+  try {
+    if (registryService) {
+      registryId = registryService->_cxx_register(serviceOffer);
+    } else {
+      std::cout << "Nao foi possivel adquirir um proxy para o servico de registro." << std::endl;
+      exit(1);
+    }
+  } catch (const tecgraf::openbus::core::v1_05::registry_service::UnathorizedFacets &e) {
+    std::cout << "Nao foi possivel registrar IHello." << std::endl;
+    for (CORBA::ULong idx = 0; idx < e.facets.length(); ++idx)
+      std::cout << "Faceta nao autorizada: " << e.facets[idx] << std::endl;
+    exit(1);
+  }
+  std::cout << "Servico IHello registrado." << std::endl;
+}
+
 struct RenewLogin : public openbus::Openbus::LeaseExpiredCallback {
   void expired() {
     try {
       bus->disconnect();
-      bus->connect(entityName, privateKeyFilename, ACSCertificateFilename);
-    } catch (const openbus::LOGIN_FAILURE &) {
-      std::cout << "Falha no login." << std::endl;
+      loginAndRegister();
     } catch (const CORBA::Exception &) {
       std::cout << "Nao foi possivel desconectar e conectar novamente." << std::endl;
     }
@@ -65,84 +124,11 @@ int main(int argc, char **argv) {
   RenewLogin renewLogin;
   signal(SIGINT, termination_handler);
   bus = openbus::Openbus::getInstance();
-
   commandLineParse(argc, argv);
-
   bus->init(argc, argv);
-
-  std::cout << "Conectando no barramento..." << std::endl;
-
-/* Conexao com o barramento atraves de certificado. */
-  try {
-    registryService = bus->connect(entityName, privateKeyFilename, ACSCertificateFilename);
-  } catch (const CORBA::SystemException &) {
-    std::cout << "** Nao foi possivel se conectar ao barramento. **" << std::endl
-              << "* Falha na comunicacao. *" << std::endl;
-    exit(1);
-  } catch (const openbus::LOGIN_FAILURE &) {
-    std::cout << "** Nao foi possivel se conectar ao barramento. **" << std::endl
-              << "* Par usuario/senha invalido. *" << std::endl;
-    exit(1);
-  } catch (const openbus::SECURITY_EXCEPTION &e) {
-    std::cout << e.what() << std::endl;
-    exit(1);
-  }
-
   bus->setLeaseExpiredCallback(&renewLogin);
-
-  std::cout << "Conexao com o barramento estabelecida com sucesso!" << std::endl;
-
-/* Definicao do componente. */
-  scs::core::ComponentId componentId;
-  componentId.name = "HelloComponent";
-  componentId.major_version = '1';
-  componentId.minor_version = '0';
-  componentId.patch_version = '0';
-  componentId.platform_spec = "nenhuma";
-
-  ::componentContext = new scs::core::ComponentContext(bus->getORB(), componentId);
-
-  std::auto_ptr<PortableServer::ServantBase> facet(new HelloImpl);
-  ::componentContext->addFacet(facetName, "IDL:demoidl/hello/IHello:1.0", facet);
-  assert(!facet.get());
-
-// /* Descricao das facetas. */
-//   std::list<scs::core::ExtendedFacetDescription> extFacets;
-//   scs::core::ExtendedFacetDescription helloDesc;
-//   helloDesc.name = facetName;
-//   helloDesc.interface_name = "IDL:demoidl/hello/IHello:1.0";
-//   helloDesc.instantiator = HelloImpl::instantiate;
-//   helloDesc.destructor = HelloImpl::destruct;
-//   extFacets.push_back(helloDesc);
-//   componentContext = componentBuilder->newComponent(extFacets, componentId);
-
-  openbus::util::PropertyListHelper propertyListHelper;
-
-/* Criacao de uma *oferta de servico*. */
-  tecgraf::openbus::core::v1_05::registry_service::ServiceOffer serviceOffer;
-  serviceOffer.properties = propertyListHelper.getPropertyList();
-  serviceOffer.member = componentContext->getIComponent();
-
-  std::cout << "Registrando servico IHello no barramento..." << std::endl;
-
-/* Registro do servico no barramento. */
-  try {
-    if (registryService) {
-      registryId = registryService->_cxx_register(serviceOffer);
-    } else {
-      std::cout << "Nao foi possivel adquirir um proxy para o servico de registro." << std::endl;
-      exit(1);
-    }
-  } catch (const tecgraf::openbus::core::v1_05::registry_service::UnathorizedFacets &e) {
-    std::cout << "Nao foi possivel registrar IHello." << std::endl;
-    for (CORBA::ULong idx = 0; idx < e.facets.length(); ++idx)
-      std::cout << "Faceta nao autorizada: " << e.facets[idx] << std::endl;
-    exit(1);
-  }
-  std::cout << "Servico IHello registrado." << std::endl;
+  loginAndRegister();
   std::cout << "Aguardando requisicoes..." << std::endl;
-
   bus->run();
-
   return 0;
 }
