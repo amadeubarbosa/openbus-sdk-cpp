2183bd7fb53b29b40acc6e5d64b7e6f7b287aae2
Felipe Magno Almeida <felipema@tecgraf.puc-rio.br>
2012-10-19 18:16:40 +0000

    [OPENBUS-1547]: Incluir as funcionalidades do Helper (Assistant) no SDK C++  - Demo independent clock adaptado para assistant
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@134590 ae0415b3-e90b-0410-900d-d0be9363c56b

--- assistant/demo/independent_clock/client/client.cpp
+++ assistant/demo/independent_clock/client/client.cpp
@@ -1,5 +1,4 @@
-#include <openbus/ConnectionManager.h>
-#include <openbus/ORBInitializer.h>
+#include <openbus/assistant.h>
 
 #include <iostream>
 
@@ -13,55 +12,75 @@ namespace access_control
  = tecgraf::openbus::core::v2_0::services::access_control;
 
 template <typename F>
-bool call_with_found_clock(offer_registry::ServiceOfferDescSeq_var offers, F f)
+bool call_with_found_clock(offer_registry::ServiceOfferDescSeq offers, F f)
 {
-  if (offers->length() == 0)
+  try
   {
-    std::cout << "O servico Clock nao se encontra no barramento." << std::endl;
-    return false;
-  }
-  else if(offers->length() == 1)
-  {
-    CORBA::ULong i = 0;
-    demo::Clock_var hello = demo::Clock::_narrow
-      (offers[i].service_ref->getFacetByName("hello"));
-    if(!CORBA::is_nil(hello))
-      f(hello);
-  }
-  else
-  {
-    std::cout << "Existe mais de um servico Clock no barramento. Tentaremos encontrar uma funcional." << std::endl;
-
-    for(CORBA::ULong i = 0; i != offers->length(); ++i)
+    if (offers.length() == 0)
     {
-      try
+      std::cout << "O servico Clock nao se encontra no barramento." << std::endl;
+      return false;
+    }
+    else if(offers.length() == 1)
+    {
+      CORBA::ULong i = 0;
+      demo::Clock_var clock = demo::Clock::_narrow
+        (offers[i].service_ref->getFacetByName("clock"));
+      if(!CORBA::is_nil(clock))
       {
-        CORBA::Object_var o = offers[i].service_ref
-          ->getFacetByName("clock");
-        demo::Clock_var clock = demo::Clock::_narrow(o);
-        if(!CORBA::is_nil(clock))
-        {
-          f(clock);
-          return false;
-        }
-      }
-      catch (CORBA::TRANSIENT const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::OBJECT_NOT_EXIST const&)
-      {
-        std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
-      }
-      catch (CORBA::COMM_FAILURE const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
+        f(clock);
+        return true;
       }
     }
-    return false;
+    else
+    {
+      std::cout << "Existe mais de um servico Clock no barramento. Tentaremos encontrar uma funcional." << std::endl;
+
+      for(CORBA::ULong i = 0; i != offers.length(); ++i)
+      {
+        try
+        {
+          CORBA::Object_var o = offers[i].service_ref
+            ->getFacetByName("clock");
+          demo::Clock_var clock = demo::Clock::_narrow(o);
+          if(!CORBA::is_nil(clock))
+          {
+            f(clock);
+            return true;
+          }
+        }
+        catch (CORBA::TRANSIENT const&)
+        {
+          std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
+            "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
+        }
+        catch (CORBA::OBJECT_NOT_EXIST const&)
+        {
+          std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
+        }
+        catch (CORBA::COMM_FAILURE const&)
+        {
+          std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
+            "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
+        }
+      }
+    }
   }
+  catch (CORBA::TRANSIENT const&)
+  {
+    std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
+      "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
+  }
+  catch (CORBA::OBJECT_NOT_EXIST const&)
+  {
+    std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
+  }
+  catch (CORBA::COMM_FAILURE const&)
+  {
+    std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
+      "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
+  }
+  
   return false;
 }
 
@@ -76,118 +95,24 @@ struct printTime
   }
 };
 
-struct onReloginCallback
-{
-  typedef void result_type;
-  result_type operator()(openbus::Connection& c, access_control::LoginInfo info) const
-  {
-    do
-    {
-      try
-      {
-        c.loginByPassword("demo", "demo");
-        break;
-      }
-      catch(tecgraf::openbus::core::v2_0::services::access_control::AccessDenied const& e)
-      {
-        std::cout << "Falha ao tentar realizar o login por senha no barramento: "
-          "a entidade já está com o login realizado. Esta falha será ignorada." << std::endl;
-      }
-      catch (services::ServiceFailure e)
-      {
-        std::cout << "Falha no serviço remoto. Causa: " << std::endl;
-      }
-      catch (CORBA::TRANSIENT const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::COMM_FAILURE const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::OBJECT_NOT_EXIST const&)
-      {
-        std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
-      }
-      unsigned int t = 30u;
-      do { t = sleep(t); } while(t);
-    }
-    while(true);
-  }
-};
-
 int main(int argc, char** argv)
 {
-  // Inicializando CORBA e ativando o RootPOA
-  CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
-  CORBA::Object_var o = orb->resolve_initial_references("RootPOA");
-  PortableServer::POA_var poa = PortableServer::POA::_narrow(o);
-  assert(!CORBA::is_nil(poa));
-  PortableServer::POAManager_var poa_manager = poa->the_POAManager();
-  poa_manager->activate();
-
-  // Construindo e logando conexao
-  openbus::ConnectionManager* manager = dynamic_cast<openbus::ConnectionManager*>
-    (orb->resolve_initial_references(CONNECTION_MANAGER_ID));
-  assert(manager != 0);
-  std::auto_ptr <openbus::Connection> conn;
+  using namespace openbus::assistant::keywords;
+  openbus::assistant::Assistant assistant
+    ("localhost", 2089, _username = "demo", _password = "demo"
+     , _argc = argc, _argv = argv);
 
   do
   {
+    bool s = false;
     try
     {
-      conn = manager->createConnection("localhost", 2089);
-      conn->onInvalidLogin( ::onReloginCallback());
-      conn->loginByPassword("demo", "demo");
-      manager->setDefaultConnection(conn.get());
-      break;
-    }
-    catch(tecgraf::openbus::core::v2_0::services::access_control::AccessDenied const& e)
-    {
-      std::cout << "Falha ao tentar realizar o login por senha no barramento: "
-        "a entidade já está com o login realizado. Esta falha será ignorada." << std::endl;
-    }
-    catch (services::ServiceFailure e)
-    {
-      std::cout << "Falha no serviço remoto. Causa: " << std::endl;
-    }
-    catch (CORBA::TRANSIENT const&)
-    {
-      std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-        "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-    }
-    catch (CORBA::COMM_FAILURE const&)
-    {
-      std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-        "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-    }
-    catch (CORBA::OBJECT_NOT_EXIST const&)
-    {
-      std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
-    }
-    unsigned int t = 30u;
-    do { t = sleep(t); } while(t);
-  }
-  while(true);
-
-  // Recebendo ofertas
-  openbus::idl_or::ServicePropertySeq props;
-  props.length(2);
-  props[0].name  = "openbus.offer.entity";
-  props[0].value = "demo";
-  props[1].name  = "openbus.component.facet";
-  props[1].value = "clock";
-  
-  do
-  {
-    try
-    {
-      offer_registry::ServiceOfferDescSeq_var offers = conn->offers()->findServices(props);
+      offer_registry::ServiceOfferDescSeq offers
+        = assistant.findOffers(assistant.createFacetAndEntityProperty("clock", "demo"), -1);
       // Pegando uma oferta valida
       if(! ::call_with_found_clock(offers, printTime()))
       {
+        s = true;
         std::cout << "Servidor fora do ar, hora atual " << time(0) << std::endl;
       }
     }
@@ -209,6 +134,12 @@ int main(int argc, char** argv)
       std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
         "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
     }
+
+    if(!s)
+    {
+      std::cout << "Barramento fora do ar, hora atual " << time(0) << std::endl;
+    }
+
     unsigned int t = 30u;
     do { t = sleep(t); } while(t);
   }
--- assistant/demo/independent_clock/server/server.cpp
+++ assistant/demo/independent_clock/server/server.cpp
@@ -1,5 +1,4 @@
-#include <openbus/ConnectionManager.h>
-#include <openbus/ORBInitializer.h>
+#include <openbus/assistant.h>
 #include <scs/ComponentContext.h>
 #include <iostream>
 
@@ -33,48 +32,6 @@ void run_orb(CORBA::ORB_var orb)
   orb->run();
 }
 
-struct onReloginCallback
-{
-  typedef void result_type;
-  result_type operator()(openbus::Connection& c, access_control::LoginInfo info) const
-  {
-    do
-    {
-      try
-      {
-        c.loginByPassword("demo", "demo");
-        break;
-      }
-      catch(tecgraf::openbus::core::v2_0::services::access_control::AccessDenied const& e)
-      {
-        std::cout << "Falha ao tentar realizar o login por senha no barramento: "
-          "a entidade já está com o login realizado. Esta falha será ignorada." << std::endl;
-      }
-      catch (services::ServiceFailure e)
-      {
-        std::cout << "Falha no serviço remoto. Causa: " << std::endl;
-      }
-      catch (CORBA::TRANSIENT const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::COMM_FAILURE const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::OBJECT_NOT_EXIST const&)
-      {
-        std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
-      }
-      unsigned int t = 30u;
-      do { t = sleep(t); } while(t);
-    }
-    while(true);
-  }
-};
-
 void clock_loop()
 {
   while(true)
@@ -87,62 +44,17 @@ void clock_loop()
 
 int main(int argc, char** argv)
 {
-  CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
-  CORBA::Object_var o = orb->resolve_initial_references("RootPOA");
-  PortableServer::POA_var poa = PortableServer::POA::_narrow(o);
-  assert(!CORBA::is_nil(poa));
-  PortableServer::POAManager_var poa_manager = poa->the_POAManager();
-  poa_manager->activate();
+  using namespace openbus::assistant::keywords;
+  openbus::assistant::Assistant assistant
+    ("localhost", 2089, _username = "demo", _password = "demo"
+     , _argc = argc, _argv = argv);
 
-  boost::thread orb_thread(boost::bind(&run_orb, orb));
-
-  // Construindo e logando conexao
-  openbus::ConnectionManager* manager = dynamic_cast<openbus::ConnectionManager*>
-    (orb->resolve_initial_references(CONNECTION_MANAGER_ID));
-  assert(manager != 0);
-  std::auto_ptr <openbus::Connection> conn;
-  do
-  {
-    try
-    {
-      conn = manager->createConnection("localhost", 2089);
-      conn->onInvalidLogin( ::onReloginCallback());
-      conn->loginByPassword("demo", "demo");
-      manager->setDefaultConnection(conn.get());
-      break;
-    }
-    catch(tecgraf::openbus::core::v2_0::services::access_control::AccessDenied const& e)
-    {
-      std::cout << "Falha ao tentar realizar o login por senha no barramento: "
-        "a entidade já está com o login realizado. Esta falha será ignorada." << std::endl;
-    }
-    catch (services::ServiceFailure e)
-    {
-      std::cout << "Falha no serviço remoto. Causa: " << std::endl;
-    }
-    catch (CORBA::TRANSIENT const&)
-    {
-      std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-        "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-    }
-    catch (CORBA::COMM_FAILURE const&)
-    {
-      std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-        "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-    }
-    catch (CORBA::OBJECT_NOT_EXIST const&)
-    {
-      std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
-    }
-    unsigned int t = 30u;
-    do { t = sleep(t); } while(t);
-  }
-  while(true);
+  boost::thread orb_thread(boost::bind(&run_orb, assistant.orb()));
 
   scs::core::ComponentId componentId
     = { "IndependentClock", '1', '0', '0', ""};
   scs::core::ComponentContext clock_component
-    (manager->orb(), componentId);
+    (assistant.orb(), componentId);
   ClockImpl clock_servant;
   clock_component.addFacet
     ("clock", demo::_tc_Clock->id(), &clock_servant);
@@ -150,59 +62,10 @@ int main(int argc, char** argv)
   properties.length(1);
   properties[0].name = "offer.domain";
   properties[0].value = "Demos";
+  assistant.addOffer(clock_component.getIComponent(), properties);
 
   boost::thread local_clock_thread(& ::clock_loop);
 
-  try
-  {
-    do
-    {
-      try
-      {
-        conn->offers()->registerService(clock_component.getIComponent(), properties);
-        break;
-      }
-      catch(tecgraf::openbus::core::v2_0::services::access_control::AccessDenied const& e)
-      {
-        std::cout << "Falha ao tentar realizar o login por senha no barramento: "
-          "a entidade já está com o login realizado. Esta falha será ignorada." << std::endl;
-      }
-      catch (services::ServiceFailure const& e)
-      {
-        std::cout << "Falha no servico remoto. Causa: " << std::endl;
-      }
-      catch (offer_registry::UnauthorizedFacets const& e)
-      {
-        std::cout << "Faceta nao autorizada no barramento: " << std::endl;
-      }
-      catch (CORBA::TRANSIENT const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::COMM_FAILURE const&)
-      {
-        std::cout << "Erro de comunicacao. Verifique se o sistema se encontra "
-          "ainda disponivel ou se sua conexao com o mesmo foi interrompida" << std::endl;
-      }
-      catch (CORBA::OBJECT_NOT_EXIST const&)
-      {
-        std::cout << "Objeto remoto nao existe mais. Verifique se o sistema se encontra disponivel" << std::endl;
-      }
-      unsigned int t = 30u;
-      do { t = sleep(t); } while(t);
-    }
-    while(true);
-  
-    orb_thread.join();
-    local_clock_thread.join();
-  }
-  catch(offer_registry::InvalidService const&)
-  {
-    std::cout << "Barramento diz que o servico ofertado nao e um componente valido" << std::endl;
-  }
-  catch(offer_registry::InvalidProperties const&)
-  {
-    std::cout << "Barramento diz que o servico ofertado possui ofertas" << std::endl;
-  }
+  orb_thread.join();
+  local_clock_thread.join();
 }
