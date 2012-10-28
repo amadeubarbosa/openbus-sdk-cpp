fb5e2f5cf72cc090465c2deaba0be8bfd88aa483
Felipe Magno Almeida <felipema@tecgraf.puc-rio.br>
2012-10-28 16:52:27 +0000

    [OPENBUS-1547]: Incluir as funcionalidades do Helper (Assistant) no SDK C++  - Documentação do assistant
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@134893 ae0415b3-e90b-0410-900d-d0be9363c56b

---
+++ assistant/doc/searching_offers.cpp
@@ -0,0 +1,31 @@
+
+//[searching_offers
+//[searching_offers_include
+#include <openbus/assistant.h>
+//]
+
+//[searching_offers_include_hello
+#include <stubs/hello.h>
+//]
+
+#include <iostream>
+
+//[searching_offers_auth
+int main()
+{
+  using namespace openbus::assistant::keywords;
+  openbus::assistant::Assistant assistant
+    ("localhost", 2089, _username = "usuario"
+     , _password = "senha");
+  //]
+  //[searching_offers_find_service
+  using tecgraf::openbus::core::v2_0::services::offer_registry::ServiceOfferDescSeq;
+  ServiceOfferDescSeq offers = assistant.findServices(assistant.createFacetAndEntityProperty("hello", "usuario"), -1);
+  //]
+  //[searching_offers_use_service
+  for(CORBA::ULong i = 0; i != offers.length(); ++i)
+    if( ::Hello_var p = ::Hello::_narrow(offers[i].service_ref))
+      p->sayHello();
+  //]
+}
+//]
---
+++ assistant/doc/searching_offers_errors.cpp
@@ -0,0 +1,35 @@
+
+//[searching_offers_errors
+//[searching_offers_errors_include
+#include <openbus/assistant.h>
+//]
+
+//[searching_offers_errors_include_hello
+#include <stubs/hello.h>
+//]
+
+#include <iostream>
+
+//[searching_offers_errors_auth
+int main()
+{
+  using namespace openbus::assistant::keywords;
+  openbus::assistant::Assistant assistant
+    ("localhost", 2089, _username = "usuario"
+     , _password = "senha");
+  //]
+  //[searching_offers_errors_find_service
+  using tecgraf::openbus::core::v2_0::services::offer_registry::ServiceOfferDescSeq;
+  ServiceOfferDescSeq offers = assistant.findServices(assistant.createFacetAndEntityProperty("hello", "usuario"), -1);
+  //]
+  //[searching_offers_errors_use_service
+  for(CORBA::ULong i = 0; i != offers.length(); ++i)
+    try
+    {
+      if( ::Hello_var p = ::Hello::_narrow(offers[i].service_ref))
+        p->sayHello();
+    }
+    catch(CORBA::SystemException const&) {}
+  //]
+}
+//]
