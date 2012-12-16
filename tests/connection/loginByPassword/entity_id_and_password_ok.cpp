a76c9a6492cb50ea5acea83de5c6d7a65f71afb5
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2012-12-16 22:36:28 +0000

    OPENBUS-2149 Usar cfg.host() e cfg.port() nos testes
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@136431 ae0415b3-e90b-0410-900d-d0be9363c56b

--- tests/connection/loginByPassword/entity_id_and_password_ok.cpp
+++ tests/connection/loginByPassword/entity_id_and_password_ok.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv)
   CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
   CORBA::Object_ptr obj_connection_manager = orb->resolve_initial_references("OpenBusContext");
   openbus::OpenBusContext* openbusContext = dynamic_cast<openbus::OpenBusContext*>(obj_connection_manager);
-  std::auto_ptr <openbus::Connection> conn (openbusContext->createConnection("localhost", 2089));
+  std::auto_ptr <openbus::Connection> conn (openbusContext->createConnection(cfg.host().c_str(), 
+                                                                             cfg.port()));
   conn->loginByPassword(cfg.user().c_str(), cfg.password().c_str());
 }
--- tests/connection/loginByPassword/wrong_password.cpp
+++ tests/connection/loginByPassword/wrong_password.cpp
@@ -8,7 +8,8 @@ int main(int argc, char** argv)
   CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
   CORBA::Object_ptr obj_connection_manager = orb->resolve_initial_references("OpenBusContext");
   openbus::OpenBusContext* openbusContext = dynamic_cast<openbus::OpenBusContext*>(obj_connection_manager);
-  std::auto_ptr <openbus::Connection> conn (openbusContext->createConnection("localhost", 2089));
+  std::auto_ptr <openbus::Connection> conn (openbusContext->createConnection(cfg.host().c_str(), 
+                                                                             cfg.port()));
   try
   {
     conn->loginByPassword(cfg.user().c_str(), (cfg.password() + "WRONG").c_str());
