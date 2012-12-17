4387301512df0b038453d245da36457cb5260795
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2012-12-17 02:28:15 +0000

    OPENBUS-2150 Refatoracao do SDK C++ 2.0.0
    + Arquivos locais adicionados indevidamente.
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@136438 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/delegation/gen-stubs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p stubs
-cd stubs 
-idl --no-paths --poa --typecode ../idl/messages.idl
--- interop/multiplexing/gen-stubs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p stubs
-cd stubs 
-idl --no-paths --poa ../idl/hello.idl
