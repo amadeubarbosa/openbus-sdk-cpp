ebafa8fae1da74abc424dcbd5c3b74ad2d038951
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2013-01-15 15:36:24 +0000

    OPENBUS-2150 Refatoracao do SDK C++ 2.0.0
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/openbus_v2_proto@137101 ae0415b3-e90b-0410-900d-d0be9363c56b

--- tests/connection/loginByCertificate/entity_id_and_file_private_key_ok.cpp
+++ tests/connection/loginByCertificate/entity_id_and_file_private_key_ok.cpp
@@ -12,8 +12,12 @@ int main(int argc, char** argv)
   openbus::configuration cfg(argc, argv);
   assert(argc >= 2); // Check that there's at least one argument passed
   CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
-  CORBA::Object_ptr obj_connection_manager = orb->resolve_initial_references("OpenBusContext");
-  openbus::OpenBusContext* openbusContext = dynamic_cast<openbus::OpenBusContext*>(obj_connection_manager);
-  std::auto_ptr <openbus::Connection> conn (openbusContext->createConnection(cfg.host(), cfg.port()));
-  conn->loginByCertificate(cfg.certificate_user().c_str(), openbus::PrivateKey(argv[1]));
+  CORBA::Object_ptr obj_connection_manager = 
+    orb->resolve_initial_references("OpenBusContext");
+  openbus::OpenBusContext* openbusContext = 
+    dynamic_cast<openbus::OpenBusContext *> (obj_connection_manager);
+  std::auto_ptr <openbus::Connection> 
+    conn (openbusContext->createConnection(cfg.host(), cfg.port()));
+  conn->loginByCertificate(cfg.certificate_user(), 
+                           openbus::PrivateKey(argv[argc-1]));
 }
