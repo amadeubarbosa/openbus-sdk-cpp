40bae01d821d182d0354477dd26a0e831cf48756
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2013-06-25 17:42:27 +0000

    [OPENBUS-2302] Uso de boost::regex para processar o arquivo de propriedades do simple interop
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/02_00_00@142161 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/simple/src/client.cpp
+++ interop/simple/src/client.cpp
@@ -5,59 +5,57 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include <boost/regex.hpp>
+#include <boost/lexical_cast.hpp>
 #include "stubs/hello.h"
 
 const std::string entity("interop_hello_cpp_client");
 
-template <typename T>
-void read(std::ifstream &in, T &val, const std::string &param)
+std::string host("localhost");
+unsigned short port = 2089;
+bool regex_cb(const boost::match_results<std::string::const_iterator> &what)
 {
-  std::string arg;
-  while (in >> arg) 
+  if ("bust.host.name" == what[1])
   {
-    if (arg == param) 
-    {
-      char c;
-      in >> c;
-      if (c == '=') 
-      {
-        if (!(in >> val)) 
-        {
-          assert(0);
-        }
-      } 
-      else 
-      {
-        assert(0);
-      }
-    } 
+    host = what[2];
   }
+  else if ("bus.host.port" == what[1])
+  {
+    port = boost::lexical_cast<unsigned short>(what[2]);
+  }
+  return true;
 }
 
 int main(int argc, char **argv) 
 {
-  std::string host("localhost");
-  unsigned short port = 2089;
-  std::ifstream config("test.properties", std::ifstream::in);
-  assert(config);
-  read(config, host, "bus.host.name");
-  read(config, port, "bus.host.port");
-  config.close();
-
+  std::string prop;
+  {
+    std::ifstream in("test.properties");
+    assert(in);
+    in.seekg(0, std::ios::end);
+    prop.resize(in.tellg());
+    in.seekg(0, std::ios::beg);
+    in.read(&prop[0], prop.size());
+  }
+  boost::regex exp("([\\w\\.]+)\\s*=\\s*(\\w+)");
+  boost::sregex_iterator m1(prop.begin(), prop.end(), exp), m2;
+  std::for_each(m1, m2, &regex_cb);
   try
   {
     openbus::log().set_level(openbus::debug_level);
-    CORBA::ORB_ptr orb = openbus::ORBInitializer(argc, argv);
+    CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
     CORBA::Object_var o = orb->resolve_initial_references("RootPOA");
     PortableServer::POA_var poa = PortableServer::POA::_narrow(o);
     assert(!CORBA::is_nil(poa));
     PortableServer::POAManager_var poa_manager = poa->the_POAManager();
     poa_manager->activate();
-    openbus::OpenBusContext *busCtx = dynamic_cast<openbus::OpenBusContext *>
+    openbus::OpenBusContext *ctx = dynamic_cast<openbus::OpenBusContext *>
       (orb->resolve_initial_references("OpenBusContext"));
-    std::auto_ptr <openbus::Connection> conn (busCtx->createConnection(host, 
-                                                                       port));
-    busCtx->setDefaultConnection(conn.get());
+    std::auto_ptr<openbus::Connection> conn(ctx->createConnection(host, port));
+    ctx->setDefaultConnection(conn.get());
     conn->loginByPassword(entity, entity);
     openbus::idl_or::ServicePropertySeq props;
     props.length(2);
@@ -67,7 +65,7 @@ int main(int argc, char **argv)
     props[i+1].name  = "openbus.component.interface";
     props[i+1].value = "IDL:tecgraf/openbus/interop/simple/Hello:1.0";
     openbus::idl_or::ServiceOfferDescSeq_var offers = 
-      busCtx->getOfferRegistry()->findServices(props);
+      ctx->getOfferRegistry()->findServices(props);
     for (CORBA::ULong idx = 0; idx != offers->length(); ++idx) 
     {
       CORBA::Object_var o = offers[idx].service_ref->getFacetByName("Hello");
@@ -75,7 +73,10 @@ int main(int argc, char **argv)
         tecgraf::openbus::interop::simple::Hello::_narrow(o);
       const char *msg = hello->sayHello();
       std::string s = "Hello " + entity + "!";
-      assert(msg == s);
+      if (!(msg == s))
+      {
+        throw std::runtime_error("msg != s");
+      }
     }
   } 
   catch (const CORBA::Exception &e) 
