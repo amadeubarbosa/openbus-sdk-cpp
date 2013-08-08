031792c60b03a77bbbd89e227878f5a29b016962
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2013-08-08 13:25:20 +0000

    [OPENBUS-2337] Uso de boost::program_options para carregar as propriedades dos interops através da linha de comando ou do arquivo test.properties [OPENBUS-2343] Criar Jamfile e descritor Puts para interop sharedauth
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/02_00_00@143148 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/sharedauth/src/client.cpp
+++ interop/sharedauth/src/client.cpp
@@ -1,46 +1,69 @@
 // -*- coding: iso-8859-1-unix -*-
+
+#include "stubs/hello.h"
+#include "stubs/encoding.h"
 #include <openbus/ORBInitializer.hpp>
 #include <openbus/log.hpp>
 #include <openbus/OpenBusContext.hpp>
-#include <iostream>
-#include "stubs/hello.h"
-#include "stubs/encoding.h"
-#include "properties_reader.h"
 
-namespace access_control
- = tecgraf::openbus::core::v2_0::services::access_control;
+#include <iostream>
+#include <fstream>
+#include <boost/program_options.hpp>
+
 namespace sharedauth = tecgraf::openbus::interop::sharedauth;
 
+const std::string entity("interop_sharedauth_cpp_client");
+std::string bus_host;
+unsigned short bus_port;
+
+void load_options(int argc, char **argv)
+{
+  namespace po = boost::program_options;
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "Help")
+    ("bus.host.name", po::value<std::string>()->default_value("localhost"),
+     "Host to OpenBus")
+    ("bus.host.port", po::value<unsigned short>()->default_value(2089), 
+     "Port to OpenBus");
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::store(po::parse_config_file<char>("test.properties", desc), vm);
+  po::notify(vm);
+  if (vm.count("help")) 
+  {
+    std::cout << desc << std::endl;
+    std::exit(1);
+  }
+  if (vm.count("bus.host.name"))
+  {
+    bus_host = vm["bus.host.name"].as<std::string>();
+  }
+  if (vm.count("bus.host.port"))
+  {
+    bus_port = vm["bus.host.port"].as<unsigned short>();
+  }
+}
+
 int main(int argc, char** argv) {
-  try {
-    openbus::log.set_level(openbus::debug_level);
+  try 
+  {
+    load_options(argc, argv);
 
-    ::properties properties_file;
-    if(properties_file.openbus_log_file.empty())
-    {
-      throw std::runtime_error("Nenhum arquivo de log foi especificado na configuracao");
-    }
-
-    if(properties_file.buses.size() < 1)
-      throw std::runtime_error("No bus is configured");
-
-    ::properties::bus bus = properties_file.buses[0];
-
-    CORBA::ORB *orb = openbus::ORBInitializer(argc, argv);
+    CORBA::ORB_var orb = openbus::ORBInitializer(argc, argv);
     CORBA::Object_var o = orb->resolve_initial_references("RootPOA");
     PortableServer::POA_var poa = PortableServer::POA::_narrow(o);
     assert(!CORBA::is_nil(poa));
     PortableServer::POAManager_var poa_manager = poa->the_POAManager();
     poa_manager->activate();
 
-    openbus::OpenBusContext *openbusContext = dynamic_cast<openbus::OpenBusContext*>
+    openbus::OpenBusContext *const ctx = dynamic_cast<openbus::OpenBusContext*>
       (orb->resolve_initial_references("OpenBusContext"));
     std::auto_ptr <openbus::Connection> conn
-      (openbusContext->createConnection(bus.host, bus.port));
+      (ctx->createConnection(bus_host, bus_port));
 
-    openbusContext->setDefaultConnection(conn.get());
-    conn->loginByPassword("interop_sharedauth_cpp_client"
-                          , "interop_sharedauth_cpp_client");
+    ctx->setDefaultConnection(conn.get());
+    conn->loginByPassword(entity, entity);
 
     {
       std::pair <openbus::idl_ac::LoginProcess_ptr, openbus::idl::OctetSeq> credential
@@ -63,7 +86,7 @@ int main(int argc, char** argv) {
       any <<= sharedauth;
       CORBA::OctetSeq_var secret_seq = codec->encode_value(any);
 
-      std::ofstream file(properties_file.openbus_log_file.c_str());
+      std::ofstream file(".secret");
       std::copy(secret_seq->get_buffer()
                 , secret_seq->get_buffer() + secret_seq->length()
                 , std::ostream_iterator<char>(file));
@@ -73,30 +96,41 @@ int main(int argc, char** argv) {
 
     openbus::idl_or::ServicePropertySeq props;
     props.length(2);
-    props[0].name  = "openbus.component.facet";
-    props[0].value = "Hello";
-    props[1].name  = "offer.domain";
-    props[1].value = "Interoperability Tests";
+    props[static_cast<CORBA::ULong>(0)].name  = "openbus.component.facet";
+    props[static_cast<CORBA::ULong>(0)].value = "Hello";
+    props[static_cast<CORBA::ULong>(1)].name  = "offer.domain";
+    props[static_cast<CORBA::ULong>(1)].value = "Interoperability Tests";
 
-    openbus::idl_or::ServiceOfferDescSeq_var offers = openbusContext->getOfferRegistry()->findServices(props);
+    openbus::idl_or::ServiceOfferDescSeq_var offers = 
+      ctx->getOfferRegistry()->findServices(props);
     if (offers->length())
     {
-      CORBA::Object_var o = offers[static_cast<CORBA::ULong> (0)].service_ref->getFacetByName(
-        "Hello");
+      CORBA::Object_var o = offers[static_cast<CORBA::ULong>(0)]
+        .service_ref->getFacetByName("Hello");
       tecgraf::openbus::interop::simple::Hello *hello = 
         tecgraf::openbus::interop::simple::Hello::_narrow(o);
       hello->sayHello();
-    } else std::cout << "nenhuma oferta encontrada." << std::endl;
+    } 
+    else 
+    {
+      std::cout << "nenhuma oferta encontrada." << std::endl;
+    }
     std::cout << "orb.run()" << std::endl;
-    openbusContext->orb()->run();
-  } catch(std::exception const& e) {
+    ctx->orb()->run();
+  } 
+  catch(const std::exception &e) 
+  {
     std::cout << "[error (std::exception)] " << e.what() << std::endl;
-  } catch (const CORBA::Exception &e) {
+    return -1;
+  } 
+  catch (const CORBA::Exception &e) 
+  {
     std::cout << "[error (CORBA::Exception)] " << e << std::endl;
     return -1;
-  } catch (...) {
+  } 
+  catch (...) 
+  {
     std::cout << "[error *unknow exception*]" << std::endl;
     return -1;    
   }
-  return 0;
 }
