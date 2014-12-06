9e688c6372576e599ab5c25b430a0a504c0c8472
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2014-12-06 14:39:39 +0000

    + Busca pela oferta cadastrada pelo caso de teste e nao por uma oferta similar qualquer. A correcao permite execucoes seguidas da base de testes sem efeitos colaterais para o teste get_offer enter as execucoes.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/02_00_01@159549 ae0415b3-e90b-0410-900d-d0be9363c56b

--- tests/get_offer.cpp
+++ tests/get_offer.cpp
@@ -70,23 +70,55 @@ int main(int argc, char** argv)
     ctx.addFacet("hello", "IDL:Hello:1.0", &hello_servant);
 
     openbus::idl_or::ServicePropertySeq props;
-    props.length(1);
+    props.length(2);
     openbus::idl_or::ServiceProperty property;
     property.name = "offer.domain";
     property.value = "OpenBus Demos";
     props[0] = property;
+    openbus::idl_or::ServiceProperty offer_id_prop;
+    offer_id_prop.name  = "offer.id";
+    offer_id_prop.value = conn->login()->id.in();
+    props[1] = offer_id_prop;
     openbusContext->getOfferRegistry()->registerService(ctx.getIComponent(), props);
 
-    props.length(3);
+    props.length(4);
     props[0].name  = "openbus.offer.entity";
     props[0].value = "test";
     props[1].name  = "openbus.component.facet";
     props[1].value = "hello";
     props[2].name  = "offer.domain";
     props[2].value = "OpenBus Demos";
+    props[3].name  = "offer.id";
+    props[3].value = conn->login()->id.in();
     openbus::idl_or::ServiceOfferDescSeq_var offers = openbusContext->getOfferRegistry()->findServices(props);
-    std::cout << "Returned " << offers->length() << " offers" << std::endl;
-    assert (offers->length() == 1);
+    if (offers->length() < 1)
+    {
+      std::cerr << "offers->length() < 1" << std::endl;      
+      std::abort();
+    }
+    bool found(false);
+    for (CORBA::ULong o(0); o < offers->length(); ++o)
+    {
+      for (CORBA::ULong p(0); p < offers[o].properties.length(); ++p)
+      {
+        if (offers[o].properties[p].name.in()
+            == std::string("offer.id"))
+        {
+          if (offers[o].properties[p].value.in()
+              == std::string(conn->login()->id.in()))
+          {
+            found = true;
+          }
+        }
+      }
+    }
+    if (!found)
+    {
+      std::cerr << "Nenhuma oferta encontrada com a propriedade 'offer.id' == '"
+                << conn->login()->id.in() << "'."
+                << std::endl;
+      std::abort();
+    }
     CORBA::Object_var o = (*offers)[0u].service_ref->getFacetByName("hello");
     Hello* hello = Hello::_narrow(o);
 
