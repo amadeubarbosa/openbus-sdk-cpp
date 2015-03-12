1809568c073c0d1ac306adf4a0180c147a1299dd
Ricardo Calheiros de Miranda Cosme <rcosme@tecgraf.puc-rio.br>
2015-03-12 20:53:24 +0000

    + Suporte ao MacOSX para interop/run.cpp.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/sdk/cpp/branches/02_00_01_tao@162555 ae0415b3-e90b-0410-900d-d0be9363c56b

--- interop/run.cpp
+++ interop/run.cpp
@@ -1,3 +1,5 @@
+// -*- coding: iso-8859-1-unix -*-
+
 #include <boost/system/system_error.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/process.hpp>
@@ -5,17 +7,27 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <set>
 
 using namespace boost::process;
 using namespace boost::process::initializers;
+using namespace boost::filesystem;
 
-const std::string stage_interop("../stage-interop/");
+const std::string stage_interop("/stage-interop/");
 typedef std::string flavor;
 typedef std::string flavor_tag;
+
 const std::map<flavor, flavor_tag> flavors = 
 {
+#ifdef _WIN32
   {"multithread, debug and shared", "mt-d"},
   {"multithread, release and shared", "mt"},
+#else
+  {"multithread, debug and shared", "mt-d"},
+  {"multithread, release and shared", "mt"},
+  {"multithread, debug and static", "mt-s-d"},
+  {"multithread, release and static", "mt-s"},
+#endif
 };
 
 void exec(
@@ -27,24 +39,46 @@ void exec(
 {
   boost::system::error_code ec;
   boost::iostreams::file_descriptor_sink out_sink(
+    current_path().generic_string() +
+    stage_interop + interop + "/" +
     interop + "_" + process + "-" + flavor_tag + ".out");
-  boost::filesystem::path path(stage_interop + interop + "/" +
-                               interop + "_" + process + "-" +
-                               flavor_tag + ".exe");
+  path rel_path(stage_interop + interop + "/" +
+                                   interop + "_" + process + "-" +
+                                   flavor_tag
+#ifdef _WIN32
+                                   + ".exe"
+#endif
+    );
+  path exe_path(current_path());
+  exe_path += rel_path;
+  path start_path(current_path());
+  start_path += path(stage_interop + interop);
   std::cout << "-->Running " << type << " '" << process << "'." << std::endl;
+#ifndef _WIN32
+  std::set<std::string> env;
+  env.insert("DYLD_LIBRARY_PATH=" +
+             current_path().generic_string() + "/install/deps");
+  env.insert("LD_LIBRARY_PATH=" +
+             current_path().generic_string() + "/install/deps");
+#endif
   try
   {
     child c(
       execute(
-        run_exe(path),
+        run_exe(exe_path),
         throw_on_error(),
-        start_in_dir(stage_interop + interop),
-        bind_stdout(out_sink)));
+        start_in_dir(start_path.generic_string()),
+        bind_stdout(out_sink)
+#ifdef _WIN32
+        ));
+#else
+    ,set_env(env)));
+#endif
     childs.push_back(std::move(c));
   }
   catch (const boost::system::system_error &e)
   {
-    std::cout << e.code().message() << std::endl;
+    std::cout << exe_path << ":" << e.code().message() << std::endl;
   }
 }
 
